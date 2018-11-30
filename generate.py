8550f933ea9964f20a129911ecf262187507abc2
Peter Renshaw <peterrenshaw@seldomlogical.com>
2018-12-01 09:59:00 +1100

    2018DEC01 working on generate.is_dt_fmt, mmm2num, lst2in,ex_dt, create_dt to allow building time object

--- generate.py
+++ generate.py
@@ -8,7 +8,10 @@
 # prog: pr
 # desc: generate data (at the moment Dates) so I can quickly 
 #       work with known data to use in D3 understanding, testing. 
-#       
+#
+#       fail fast on invalid data
+#
+# src: <https://docs.python.org/3/library/datetime.html#strftime-strptime-behavior>      
 # optn:       
 #       start:          start date in known format
 #                           yyyymmmddThh:mm.ss
@@ -37,6 +40,85 @@ from ast import literal_eval
 from optparse import OptionParser
 
 
+STRF_DATE_FMT_YYYYMMMDD = "%Y%b%dT%H:%M.%S"
+DATE_FORMAT_YYYYMMMDD = "YYYYMMMDDTHH:MM.SS"
+DATE_MONTH = ['JAN','FEB','MAR','APR','MAY','JUN','JUL','AUG','SEP','OCT','NOV','DEC']
+
+
+def is_dt_fmt(dt, dt_format=DATE_FORMAT_YYYYMMMDD):
+    """is supplied date in date format?"""
+    return True
+def mmm2num(mmm, months=DATE_MONTH):
+    """convert str MMM/mmm/Mmm to month index"""
+    if mmm:
+        m = mmm.upper()
+        if m in months:
+            return months.index(m) + 1
+        else:
+            return 0
+    else:
+         sys.stderr.write("\nError: mmm2num input failure. Could not find mmm <{}>".format(mmm))
+def lst2int(data, start, end):
+    """extract list data, convert to integer"""
+    if data:
+        if start >= 0 and end <= len(data):
+            d = data[start:end]
+            return int(d)
+        else:
+            sys.stderr.write("\nError: lst2int start and end selection invalid start <{}> end <{}>".format(start, end))
+    else:
+        sys.stderr.write("\nError: lst2int has no valid input data")
+def ex_dt(dt_str):
+    """extract date from string"""
+    dtd = {}
+    if is_dt_fmt(dt_str):
+        # yyyymmmddThh:mm.ss 
+        # 123456789012345678
+        year = lst2int(dt_str, 0, 4)
+        if year: dtd['year'] = year
+
+        # convert string MMM to integer 00
+        month = mmm2num(dt_str[4:7])
+        if month >= 0 and month <= 12: dtd['month'] = month
+
+        day = lst2int(dt_str, 7, 9)
+        if day: dtd['day'] = day
+
+        hour = lst2int(dt_str, 10, 12)
+        if hour: dtd['hour'] = hour
+        
+        minute = lst2int(dt_str, 13, 15)
+        if minute: dtd['minute'] = minute
+
+        second = lst2int(dt_str, 16, 18)
+        if second: dtd['second'] = second
+        
+        return dtd
+    else:
+        return dtd
+def create_dt(dtd):
+    """given dict of date info, create a date"""
+    
+    dt = datetime.datetime(dtd['year'], dtd['month'], dtd['day'])
+  
+    # timedelta
+    if dtd['hour']:
+        h = datetime.timedelta(hours=dtd['hour'])
+        dt = dt + h
+        print("hour=<{}> td=<{}>".format(h, dt.hour))
+    if dtd['minute']:
+        m = datetime.timedelta(minutes=dtd['minute'])
+        dt = dt + m
+        print("minute=<{}> td=<{}>".format(m, dt.minute))
+    if dtd['second']:
+        s = datetime.timedelta(seconds=dtd['second'])
+        dt = dt + s
+        print("seconds=<{}> td=<{}>".format(s, dt.second))
+        
+     
+    return dt    
+
+
 #======
 # main: cli entry point
 #======
@@ -49,6 +131,8 @@ def main():
                                        help="number of data points from start")
     parser.add_option("-r", "--range", dest="range",
                                        help="range of interval from start")
+    parser.add_option("-u", "--unit",  dest="unit",
+                                       help="unit of interval")
     parser.add_option("-t", "--template", dest="template",
                                        help="template string using C, printf formatting")
     parser.add_option("-j", "--json",  dest="json",
@@ -67,23 +151,31 @@ def main():
         if not len(r) == 2:
             sys.stderr.write("\nError: please supply valid range: [2018, 2019]")
 
-        # I expect two valid dates in a list
+        # I expect two valid dates as list
         start = r[0]
         end = r[1]
         print("start <{}> end <{}>".format(start, end))
-       
+         
 
         if options.json:
             print("output to json")
         else:
             print("output as py")
-    elif options.start and options.frequency:
-        print("s=<{}> f=<{}>".format(options.start, options.frequency))
+    elif options.start and options.frequency and options.unit:
+        print("s=<{}> f=<{}> u=<{}>".format(options.start, options.frequency, options.unit))
+
+        dtd = ex_dt(options.start)
+        print("dtd=<{}>".format(dtd))
+
+
+        dt = create_dt(dtd)
+        print("dt=<{}>".format(dt))
 
         if options.json:
             print("output to json")
         else:
             print("output as py")
+
     else:
         parser.print_help()      
   
